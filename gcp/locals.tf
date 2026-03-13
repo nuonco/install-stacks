213fb5f0c93f45482da720962af72b9807c2aa03
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-03-13 15:39:04 +0530

    fix: custom roles

--- gcp/locals.tf
+++ gcp/locals.tf
@@ -3,13 +3,17 @@ locals {
   region         = var.gcp_region
   role_id_prefix = replace(var.nuon_install_id, "-", "_")
 
-  has_provision    = length(var.provision_permissions) > 0 || var.provision_predefined_role != ""
-  has_maintenance  = length(var.maintenance_permissions) > 0 || var.maintenance_predefined_role != ""
-  has_deprovision  = length(var.deprovision_permissions) > 0 || var.deprovision_predefined_role != ""
+  has_provision   = length(var.provision_permissions) > 0 || var.provision_predefined_role != ""
+  has_maintenance = length(var.maintenance_permissions) > 0 || var.maintenance_predefined_role != ""
+  has_deprovision = length(var.deprovision_permissions) > 0 || var.deprovision_predefined_role != ""
 
-  has_provision_custom    = length(var.provision_permissions) > 0
-  has_maintenance_custom  = length(var.maintenance_permissions) > 0
-  has_deprovision_custom  = length(var.deprovision_permissions) > 0
+  has_provision_custom   = length(var.provision_permissions) > 0
+  has_maintenance_custom = length(var.maintenance_permissions) > 0
+  has_deprovision_custom = length(var.deprovision_permissions) > 0
+
+  # Filter to only enabled roles
+  enabled_break_glass_roles = { for k, v in var.break_glass_roles : k => v if v.enabled }
+  enabled_custom_roles      = { for k, v in var.custom_roles : k => v if v.enabled }
 
   labels = {
     "nuon-install-id" = var.nuon_install_id
