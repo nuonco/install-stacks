65e863875979bb0a1aeb7b7ff21c27d1f665a3ad
im-Amitto <29653282+im-Amitto@users.noreply.github.com>
2026-04-03 15:43:31 +0530

    chore: additional metadata fields

--- gcp/runner.tf
+++ gcp/runner.tf
@@ -7,7 +7,7 @@ resource "google_compute_instance" "runner" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
       size  = 30
       type  = "pd-balanced"
     }
@@ -22,17 +22,18 @@ resource "google_compute_instance" "runner" {
     scopes = ["cloud-platform"]
   }
 
-  metadata_startup_script = <<-EOT
-    #!/bin/bash
-    set -e
-    export NUON_RUNNER_ID=${var.runner_id}
-    export NUON_RUNNER_API_URL=${var.runner_api_url}
-    export NUON_RUNNER_API_TOKEN=${var.runner_api_token}
-    export NUON_INSTALL_ID=${var.nuon_install_id}
-    curl -fsSL ${var.runner_init_script_url} | bash
-  EOT
-
-  lifecycle {
-    ignore_changes = [metadata_startup_script]
+  metadata = {
+    nuon_runner_id      = var.runner_id
+    nuon_runner_api_url = var.runner_api_url
+    nuon_install_id     = var.nuon_install_id
+    startup-script = <<-EOT
+      #!/bin/bash
+      set -e
+      export NUON_RUNNER_ID=${var.runner_id}
+      export NUON_RUNNER_API_URL=${var.runner_api_url}
+      export NUON_RUNNER_API_TOKEN=${var.runner_api_token}
+      export NUON_INSTALL_ID=${var.nuon_install_id}
+      curl -fsSL ${var.runner_init_script_url} | bash
+    EOT
   }
 }
